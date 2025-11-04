483c2f624737eeea0a9d04811d0ebbca35c3b614
Marvin Blas <marvin.dlpg@gmail.com>
2025-11-03 22:07:43 -0500

    fisrts commit in index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', (req,res) =>{ 
-    res.send('Ejemplo running Oct 28-2025 JOSEPH MARVIN DE LA PEÑA DIAZ'); 
+    res.send('Ejemplo running Nov 3-2025 10:06pm JOSEPH MARVIN DE LA PEÑA DIAZ'); 
 }); 
 
 
